d012eceaeb25d15171cc5eecf6e1fed92b04b3ed
droneal11015 <droneal11015@gmail.com>
2019-06-06 13:07:31 -0400

    changes to master branch for deployment

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+require(`dotenv`).config();
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log('\n*** Server Running on http://localhost:${port} ***\n');
 });
+
+
+
+
+
+//step 1, check if start is in json, step 2 make sure process.env for port
